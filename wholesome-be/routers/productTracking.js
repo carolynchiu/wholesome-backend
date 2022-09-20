d6719057d7874b652a7b49170db669c6d1a47fde
Henry <henry194557@gmail.com>
2022-09-20 13:55:48 +0800

    clear

--- wholesome-be/routers/productTracking.js
+++ wholesome-be/routers/productTracking.js
@@ -2,36 +2,5 @@ const express = require("express");
 const router = express.Router();
 const pool = require("../utils/database");
 
-router.put('/:userId', async (req, res, next ) => {
-  let userId = req.params.userId
-  console.log('user',userId)
-  let productId = req.query.productId
-  console.log("productId in be", productId);
-  let isLike = req.query.isLike 
-  
-  let likeData = await pool.execute('SELECT valid AS valid FROM user_like_product WHERE  user_id = ? AND  product_id = ?',[userId, productId])
-  console.log('checkData',likeData)
-
-
-  // let [update] = await pool.execute('UPDATE user_like_product SET valid =? WHERE  user_id = ? AND  product_id = ?',[isLike, userId, productId])
-  // console.log('upDateData', update)
-
-  // let [insert] = await pool.execute('INSERT INTO user_like_product (user_id, product_id, valid) VALUES (?, ?, ?)',[userId, productId, 1])
-
-  res.json({message: 'update success'})
-})
-
-router.post('/:userId', async (req, res, next) => {
-  let userId = req.params.userId
-  console.log('user',userId)
-  let productId = req.query.productId
-  console.log("productId in be", productId);
-
-  
-
-  res.json({message:'insert success'})
-} )
-
-
 
 module.exports = router;
