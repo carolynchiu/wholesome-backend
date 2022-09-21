106eb8f51fe2989311f59449167af7e9b7cc799b
Wan <tinklin4@gmail.com>
2022-09-21 17:36:40 +0800

    finish basic part

--- wholesome-be/routers/recipeReview.js
+++ wholesome-be/routers/recipeReview.js
@@ -1,14 +1,30 @@
 const express = require('express');
+const pool = require('../utils/database');
 const router = express.Router();
 
 
-router.post('/recipeReview/${recipeId}',async(req,res,next)=>{
+router.post('/recipeReview/:recipeId',async(req,res,next)=>{
   //確認資料有沒有收到
+  console.log('review',req.body);
   //檢查userid有沒有重複
+  let [review] = await pool.execute('SELECT * FROM recipe_comment WHERE user_id =? AND recipe_id = ?',[req.body.user.id, req.params.recipeId])
+  console.log(review)
+  if (review.length > 0){
+    return res.status(400).json({message:'您已留下評論'})
+  }
+  if(req.body.reviewStar == 0){
+    return res.status(400).json({message:'請填寫星等'})
+  }
+  if(req.body.review == ''){
+    return res.status(400).json({message:'請填寫評論'})
+  }
+  let DATE = new Date().toISOString().substring(0, 10);
+  console.log (DATE)
   //資料存到資料庫
+  pool.execute('INSERT INTO recipe_comment (user_id,recipe_id,comment,grade,create_date,valid) VALUES(?,?,?,?,?,?)',[req.body.user.id, req.params.recipeId,req.body.review,req.body.reviewStar,DATE,1])
   //回覆前端
 
-  res.json({})
+  res.json({message:'ok'})
 })
 
 
--- wholesome-be/routers/recipeTracking.js
+++ wholesome-be/routers/recipeTracking.js
@@ -2,4 +2,36 @@ const express = require("express");
 const router = express.Router();
 const pool = require("../utils/database");
 
+router.get('/:recipeId/:userId',async(req,res,next)=>{
+let [likeData] = await pool.execute(
+    'SELECT * FROM user_like_recipe WHERE user_id = ? AND recipe_id = ?',[req.params.userId,req.params.recipeId]
+  )
+if(likeData.length > 0){
+    var likerecipe = true
+}else{
+    var likerecipe = false
+}
+  res.json(likerecipe)
+})
+
+router.post('/:recipeId',async(req,res,next)=>{
+    //確認資料有沒有收到
+    console.log('like',req.body);
+    //檢查userid有沒有重複
+    let [likeRecipe] = await pool.execute('SELECT * FROM user_like_recipe WHERE user_id =? AND recipe_id = ?',[req.body.user.id, req.params.recipeId])
+
+    if(likeRecipe.length > 0){
+        pool.execute('DELETE FROM user_like_recipe WHERE user_id =? AND recipe_id = ?',[req.body.user.id, req.params.recipeId])
+    }else{
+        //資料存到資料庫
+        pool.execute('INSERT INTO user_like_recipe (user_id,recipe_id) VALUES(?,?)',[req.body.user.id, req.params.recipeId])
+    }
+    
+
+    
+    //回覆前端
+  
+    res.json({message:'ok'})
+  })
+
 module.exports = router;
