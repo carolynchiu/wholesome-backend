dd61b68c05ca5663cc646b13b18312169dee02ee
Henry <henry194557@gmail.com>
2022-09-22 16:33:26 +0800

    order 註解

--- wholesome-be/routers/cart.js
+++ wholesome-be/routers/cart.js
@@ -12,30 +12,27 @@ router.post("/:userId", async (req, res) => {
   const receiver_phone= req.body.receiver.receiver_phone
   const receiver_address = req.body.receiver.receiver_address
   const total_price= req.body.cartTotalPrice
-
-  const date = dayjs(new Date().toISOString()).format('YYYYMMDD')
-  // console.log('date',date)
-
   const create_time = dayjs(new Date().toISOString()).format("YYYY-MM-DD HH:mm:ss");
 
+  // 要insert進資料庫的最新一筆orderId
   let [order_length] = await pool.execute('SELECT COUNT(*) AS total FROM order_list') 
-  // console.log(order_length[0].total)
+  const newestOrderId=order_length[0].total+1
+  const date = dayjs(new Date().toISOString()).format('YYYYMMDD')
+  const order_sn = `W`+`${date}`+"O"+`${newestOrderId}`+'U'+`${user_id}`
+  
 
-  const lastOrder=order_length[0].total+1
-
-
-  const order_sn = `W`+`${date}`+"O"+`${lastOrder}`+'U'+`${user_id}`
-  // console.log('order_sn',order_sn)
+  // insert 進 order_list
   let data = await pool.execute('INSERT INTO order_list (order_sn , user_id, coupon_id, status_id , total_price, valid, receiver_name, receiver_phone, receiver_address, create_time  ) VALUES(?,?,?,?,?,?,?,?,?,?)',[order_sn, user_id, coupon_id,3, total_price,1,receiver_name, receiver_phone, receiver_address, create_time])
  
-  const cart = req.body.cart.map((v,i)=>{return[lastOrder,v.id,v.price,v.amount]})
+  const cart = req.body.cart.map((v,i)=>{return[newestOrderId,v.id,v.price,v.amount]})
   console.log('cart array', cart)
 
+  //insert 進 order_detail
   let detail = await pool.query('INSERT INTO order_detail (order_id, product_id, product_price, amount ) VALUES ? ',[cart])
 
   
 
-  res.json({lastOrder ,message: "結帳成功" })
+  res.json({newestOrderId ,message: "結帳成功" })
 });
 
 module.exports = router;
