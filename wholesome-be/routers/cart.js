bf1ecaf49e1fe80d9234c2d10d8126bfd75ba147
Henry <henry194557@gmail.com>
2022-09-22 14:56:17 +0800

    order list half done

--- wholesome-be/routers/cart.js
+++ wholesome-be/routers/cart.js
@@ -16,12 +16,14 @@ router.post("/:userId", async (req, res) => {
   const date = dayjs(new Date().toISOString()).format('YYYYMMDD')
   console.log('date',date)
 
+  const create_time = dayjs(new Date().toISOString()).format("YYYY-MM-DD HH:mm:ss");
+
   let [order_length] = await pool.execute('SELECT COUNT(*) AS total FROM order_list') 
   console.log(order_length[0].total)
 
   const order_sn = `W`+`${date}`+"O"+`${order_length[0].total+1}`+'U'+`${user_id}`
   console.log('order_sn',order_sn)
-  let data = await pool.execute('INSERT INTO order_list (order_sn , user_id, coupon_id, status_id , total_price, valid, receiver_name, receiver_phone, receiver_address  ) VALUES(?,?,?,?,?,?,?,?,?)',[order_sn, user_id, coupon_id,3, total_price,1,receiver_name, receiver_phone, receiver_address])
+  let data = await pool.execute('INSERT INTO order_list (order_sn , user_id, coupon_id, status_id , total_price, valid, receiver_name, receiver_phone, receiver_address, create_time  ) VALUES(?,?,?,?,?,?,?,?,?,?)',[order_sn, user_id, coupon_id,3, total_price,1,receiver_name, receiver_phone, receiver_address, create_time])
 
   TODO:
 
