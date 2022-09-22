6338572ae8d8ce005aa0cce85a9cb89980ceb056
Henry <henry194557@gmail.com>
2022-09-22 16:58:14 +0800

    整理detail

--- wholesome-be/controllers/productDetail.js
+++ wholesome-be/controllers/productDetail.js
@@ -3,27 +3,18 @@ const productDetailModel = require("../models/productDetail");
 // 商品細節頁
 async function getProductDetail(req, res, next) {
  
-
-
   //前端送出請求時帶的參數:productId
   const productId = req.params.productId;
-  // console.log("productId in be", productId);
-
 
   //商品細節資料
   let productData = await productDetailModel.getSingleProduct(productId);
-  // console.log("productData", productData);
 
   let categoryId = parseInt(productData.map((v) => v.category_id));
-  // console.log('category',categoryId)
-
-
 
   //商品評分
   let productComment = await productDetailModel.getProductComment(
     productId,
   );
-  // console.log('productComment',productComment)
 
   let eachStar = productComment.map((v) => v.grade);
   let starCount = eachStar.length;
@@ -35,14 +26,9 @@ async function getProductDetail(req, res, next) {
   eachStar.length > 0
     ? (average = (totalScore / starCount).toFixed(1))
     : (average = 0);
-  // console.log("eachStar", eachStar);
-  // console.log(totalScore);
-  // console.log(starCount);
-  // console.log(average);
-
+ 
   //相關商品
   let relatedGoods = await productDetailModel.getRelatedGoods(categoryId,productId);
-  // console.log('related goods', relatedGoods)
 
   res.json({
     productData,
