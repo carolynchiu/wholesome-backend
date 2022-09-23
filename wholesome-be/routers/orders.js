1a709aa9956b2ab96bc311159246ac368555bd08
Carolyn <c920231@gmail.com>
2022-09-23 12:56:47 +0800

    update order.js

--- wholesome-be/routers/orders.js
+++ wholesome-be/routers/orders.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = express.Router();
 const pool = require("../utils/database");
 
+// 取得某筆訂單的資料
 router.get("/:orderId", async (req, res) => {
   const orderId = req.params.orderId;
   console.log(`這裡是 orderId=${orderId}的訂單頁`);
@@ -10,12 +11,13 @@ router.get("/:orderId", async (req, res) => {
     "SELECT order_list.*,coupons.name AS coupon_name, coupons.discount_price AS coupon_price FROM order_list LEFT JOIN coupons ON order_list.coupon_id =coupons.id WHERE order_list.id=?",
     [orderId]
   );
-  let [data] = await pool.execute(
+  // 訂單明細資料
+  let [orderDetaildata] = await pool.execute(
     "SELECT * FROM `order_detail` JOIN order_list ON order_detail.order_id=order_list.id JOIN products ON order_detail.product_id=products.id WHERE order_detail.order_id=?",
     [orderId]
   );
   console.log(orderData);
-  res.json({ orderData: orderData, orderDetail: data });
+  res.json({ orderData: orderData, orderDetail: orderDetaildata });
 });
 
 module.exports = router;
