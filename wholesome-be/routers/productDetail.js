5f0810989978e4b0217465417aac4396bf3402c7
Henry <henry194557@gmail.com>
2022-09-27 13:34:53 +0800

    teardown MVC of productslist and detail

--- wholesome-be/routers/productDetail.js
+++ wholesome-be/routers/productDetail.js
@@ -1,9 +1,45 @@
 const express = require("express");
 const router = express.Router();
+const pool = require('../utils/database')
 
-const detailController = require("../controllers/productDetail");
 
 // 商品細節頁
-router.get("/:productId", detailController.getProductDetail);
+router.get("/:productId", async(req,res,next) => {
+  const productId = req.params.productId;
+
+  //商品細節資料
+  let [productData] = await pool.execute("SELECT * FROM products JOIN products_category on products.category_id = products_category.category_id WHERE id = ?",[productId]);
+
+  let categoryId = parseInt(productData.map((v) => v.category_id));
+
+  //商品評分
+  let [productComment] = await pool.execute(
+    "SELECT products_comment.id, products_comment.product_id, products_comment.comment, products_comment.grade, products_comment.user_id, products_comment.time , users.name FROM products_comment INNER JOIN users ON products_comment.user_id = users.id WHERE product_id = ? ORDER BY grade DESC ",[productId]);
+
+  let eachStar = productComment.map((v) => v.grade);
+  let starCount = eachStar.length;
+
+  eachStar.length > 0
+    ? (totalScore = eachStar.reduce((p, n) => p + n))
+    : (totalScore = 0);
+
+  eachStar.length > 0
+    ? (average = (totalScore / starCount).toFixed(1))
+    : (average = 0);
+ 
+  //相關商品
+  let [relatedGoods] = await pool.execute('SELECT * FROM products WHERE category_id = ? AND id != ?',[categoryId, productId])
+
+  res.json({
+    productData,
+    stars: {
+      eachStar,
+      totalScore,
+      starCount,
+      average,
+    },
+    relatedGoods,
+  });
+});
 
 module.exports = router;
--- wholesome-be/routers/products.js
+++ wholesome-be/routers/products.js
@@ -1,9 +1,103 @@
 const express = require("express");
 const router = express.Router();
-
-const productsController = require("../controllers/products");
+const pool = require('../utils/database')
 
 // 商品列表頁
-router.get("/", productsController.getProductList);
+router.get("/", async (req, res, next) => {
+
+  let category = req.query.category;
+  req.query.search !== undefined ? search=`%${req.query.search}%` : search='%%'; 
+ 
+  console.log('search', search)
+  let page = req.query.page || 1;
+  console.log('category', category, 'page', page)
+
+  let order = req.query.order
+  console.log('order',order)
+  
+
+  //每一列表頁有幾筆資料
+  const perPage = 18;
+
+  //總筆數
+  
+  if (category === "植物奶" || category === "有機蔬果汁" || category === "營養點心") {
+       [total] = await pool.execute(
+        "SELECT COUNT(*) AS total FROM products JOIN products_category on products.category_id = products_category.category_id WHERE products_category.main_category = ? AND products.name LIKE ? ",
+        [category, search]
+      );
+
+  } else {
+       [total] = await pool.execute(
+        `SELECT COUNT(*) AS total FROM products JOIN products_category on products.category_id = products_category.category_id WHERE products_category.sub_category = ? AND products.name LIKE ? `,
+        [category, search]
+      );
+  }
+   
+  let productsTotal = total[0].total
+  console.log('productsTotal',productsTotal)
+
+  
+  let totalPage = Math.ceil(productsTotal / perPage);
+
+  // 計算offset
+  const offset = perPage * (page - 1)
+
+  if (category === "植物奶"||category === "有機蔬果汁" || category === "營養點心") {
+
+      switch(order){
+          case "價錢由高到低":
+              [data] =await pool.execute(`SELECT * FROM products JOIN products_category on products.category_id = products_category.category_id WHERE products_category.main_category = ? AND products.name LIKE ? ORDER BY products.price DESC  LIMIT ? OFFSET ?`,[category, search, perPage, offset]);
+              break;
+          case "價錢由低到高":
+              [data] = await pool.execute( `SELECT * FROM products JOIN products_category on products.category_id = products_category.category_id WHERE products_category.main_category = ? AND products.name LIKE ? ORDER BY products.price   LIMIT ? OFFSET ?`,[category, search, perPage, offset]);
+              break;
+          case "上市日期由新到舊":
+              [data] = await pool.execute(`SELECT * FROM products JOIN products_category on products.category_id = products_category.category_id WHERE products_category.main_category = ? AND products.name LIKE ? ORDER BY products.launch_time DESC  LIMIT ? OFFSET ?`,[category, search, perPage, offset])
+              break;
+          case "上市日期由舊到新":
+              [data] = await pool.execute(`SELECT * FROM products JOIN products_category on products.category_id = products_category.category_id WHERE products_category.main_category = ? AND products.name LIKE ? ORDER BY products.launch_time  LIMIT ? OFFSET ?`,[category, search, perPage, offset]);
+              break;
+          default:
+              [data] = await pool.execute(
+                `SELECT * FROM products JOIN products_category on products.category_id = products_category.category_id WHERE products_category.main_category = ? AND products.name LIKE ?  LIMIT ? OFFSET ?`,
+                [category, search, perPage, offset])
+              break;
+          }
+  } else {
+
+      switch(order){
+          case "價錢由高到低":
+              [data] = await pool.execute(`SELECT * FROM products JOIN products_category on products.category_id = products_category.category_id WHERE products_category.sub_category = ? AND products.name  LIKE ? ORDER BY products.price DESC LIMIT ? OFFSET ?`,[category, search, perPage, offset]);
+              break;
+          case "價錢由低到高":
+             [data] = await pool.execute(`SELECT * FROM products JOIN products_category on products.category_id = products_category.category_id WHERE products_category.sub_category = ? AND products.name  LIKE ? ORDER BY products.price LIMIT ? OFFSET ?`,[category, search, perPage, offset]);
+              break;
+          case "上市日期由新到舊":
+              [data] = await pool.execute(`SELECT * FROM products JOIN products_category on products.category_id = products_category.category_id WHERE products_category.sub_category = ? AND products.name LIKE ?  ORDER BY products.launch_time DESC LIMIT ? OFFSET ?`,[category, search, perPage, offset]);
+              break;
+          case "上市日期由舊到新":
+              [data] = await pool.execute( `SELECT * FROM products JOIN products_category on products.category_id = products_category.category_id WHERE products_category.sub_category = ? AND products.name  LIKE ? LIMIT ? OFFSET ?`,[category, search, perPage, offset]);
+              break;
+          default:
+              [data] = await pool.execute(`SELECT * FROM products JOIN products_category on products.category_id = products_category.category_id WHERE products_category.sub_category = ? AND products.name LIKE ? LIMIT ? OFFSET ?`,[category, search, perPage, offset]);
+              break;
+      }
+      
+  }
+  
+  // console.log('allData',data)
+  
+  res.json({
+      pagination:{
+          productsTotal,
+          perPage,
+          page,
+          totalPage,
+      },
+      data,
+  })
+  
+});
 
 module.exports = router;
